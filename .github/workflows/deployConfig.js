25bf5dc6ce474def4e453614b1e87d071e49414a
Mark Wylde <me@markwylde.com>
2022-12-11 13:25:02 +0000

    Fix CI/CD to deploy

--- .github/workflows/deployConfig.js
+++ .github/workflows/deployConfig.js
@@ -3,6 +3,7 @@ export default {
     hostname: process.env.FTP_HOSTNAME,
     username: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD,
-    source: './dist'
+    source: './dist',
+    destination: '',
   }]
 }
